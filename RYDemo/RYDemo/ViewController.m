0ff97d96bdf1eb6d3f6f0af88bf16f3a934cc8f6
RyukieSma <ryukie.sama@gmail.com>
2017-06-01 13:47:25 +0800

    no message

--- RYDemo/RYDemo/ViewController.m
+++ RYDemo/RYDemo/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "Masonry.h"
+#import "RYLabelCollection.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) UIView *viewOne;
 
 @end
 
@@ -21,6 +25,42 @@
 }
 
 - (void)setupUI {
+    self.viewOne = [[UIView alloc] init];
+    self.viewOne.backgroundColor = [UIColor cyanColor];
+    
+    RYLabelCollection *labelCollection = [[RYLabelCollection alloc] init];
+//    labelCollection.textColor = [UIColor redColor];
+    labelCollection.borderColor = [UIColor redColor];
+    labelCollection.backgroundColor = [UIColor colorWithWhite:0 alpha:0.4];
+    labelCollection.itemSpace = 10;
+    labelCollection.itemHeight = 16;
+    labelCollection.font = [UIFont systemFontOfSize:11];
+    labelCollection.maxWidth = [UIScreen mainScreen].bounds.size.width;
+    
+    labelCollection.textColorArr = @[
+                                     [UIColor redColor],
+                                     [UIColor whiteColor]
+                                     ];
+    
+    labelCollection.labels = @[
+                               @"asdfafdadfasdfasf",
+                               @"asdf",
+                               @"asdfa",
+                               @"kjnlkjnlkkljnl",
+                               @"alkjsldknjlknfgjbnkjnsdc",
+                               @"jinytinade de ad",
+                               @"asdasdasdad"
+                               ];
+    
+    [self.view addSubview:self.viewOne];
+    [self.viewOne mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.left.right.mas_equalTo(0);
+        make.height.mas_equalTo(40);
+    }];
+    [self.viewOne addSubview:labelCollection];
+    [labelCollection mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.mas_equalTo(0);
+    }];
     
 }
 
